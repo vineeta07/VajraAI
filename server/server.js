0e26ec5d60b4f7b1ea27ec2dbf455c0304488694
nnryn <kshv8@outlook.com>
2026-01-10 19:04:57 +0530

    added authentication and authorization feature

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
+const authRoutes = require("./routes/auth");
 const uploadRoutes = require("./routes/upload");
 const analyzeRoutes = require("./routes/analyze");
 const dashboardRoutes = require("./routes/dashboard");
+
+const {authenticate, authorize} = require("./middlewares/authMiddleware");
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,7 +21,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use("/api/upload", uploadRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/upload", authenticate, authorize("ADMIN"), uploadRoutes);
 app.use("/api/analyze", analyzeRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
