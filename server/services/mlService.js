fb0dbfce0ed6278ee5f90461e3dcc7ec07f73dd5
nnryn <kshv8@outlook.com>
2026-01-11 22:13:36 +0530

    changes in render.yaml

--- server/services/mlService.js
+++ server/services/mlService.js
@@ -1,8 +1,13 @@
 const axios = require("axios");
 
 async function runMLDetection(featureData) {
+  let url = process.env.ML_SERVICE_URL;
+  if (!url.startsWith("http")) {
+    url = `http://${url}/detect`;
+  }
+
   const response = await axios.post(
-    process.env.ML_SERVICE_URL,
+    url,
     featureData,
     { timeout: 10000 }
   );
