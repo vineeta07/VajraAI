883818e74b335b7c76f39bd23882ee0420c855a7
VINEETA KUMARI MEENA <viitameena117@gmail.com>
2026-05-26 04:05:09 +0530

    ccoomm

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://vajraai-backend.onrender.com',
+        target: 'https://localhost:3000',
         changeOrigin: true,
       },
     },
